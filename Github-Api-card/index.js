15be077b999e2558a8665c323440afe30a133e2f
Bharat kumar <bharatkumarm413@gmail.com>
2024-06-02 11:14:09 +0530

    github api card project done

--- Github-Api-card/index.js
+++ Github-Api-card/index.js
@@ -54,26 +54,3 @@ fetchData()
 // console.log(avtarUrl);
 
 
-// console.log("Start");
-// function sum2 (num) {
-//     return num +2
-// }
-
-
-// async function sum (){
-//     try {
-//         let result = 0;
-//          for(let i = 0; i< 10000000000; i++){
-//              result = result + 2
-//         }
-//         return result
-//     } catch (error) {
-//         console.log("Error :: ", error)
-//     }
-// }
-
-// console.log(sum2(5));
-// sum().then(data => console.log(data));
-// console.log(sum2(3));
-// console.log("My answer is comming");
-// console.log("End");
